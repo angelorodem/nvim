559ac4b46284ea6207c69594dd405ce239b612c8
Your Name <you@example.com>
2024-08-16 14:50:40 -0300

    rust

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -97,7 +97,42 @@ return {
     },
   },
   -- Rust
-  "simrat39/rust-tools.nvim",
+  {
+    "simrat39/rust-tools.nvim",
+    ft = "rust",
+    dependencies = "neovim/nvim-lspconfig",
+    opts = function()
+      return require "configs.rust-tools"
+    end,
+    config = function(_, opts)
+      require("rust-tools").setup(opts)
+    end,
+  },
   "mfussenegger/nvim-dap",
+  { "nvim-neotest/nvim-nio" },
   { "rcarriga/nvim-dap-ui", dependencies = { "mfussenegger/nvim-dap", "nvim-neotest/nvim-nio" } },
+  {
+    "saecki/crates.nvim",
+    ft = { "rust", "toml" },
+    config = function(_, opts)
+      local crates = require "crates"
+      crates.setup(opts)
+      crates.show()
+    end,
+  },
+  {
+    "hrsh7th/nvim-cmp",
+    -- opts = function()
+    --   local M = require "plugins.configs.cmp"
+    --   table.insert(M.sources, { name = "crates" })
+    --   return M
+    -- end,
+  },
+  {
+    "rust-lang/rust.vim",
+    ft = "rust",
+    config = function()
+      vim.g.rustfmt_autosave = 1
+    end,
+  },
 }
